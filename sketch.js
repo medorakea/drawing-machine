e41e8a93a6afe0949c076d9ca3db0036b92be86a
Medora Kea <medorakea@gmail.com>
2017-03-02 11:07:32 -0600

    updating opacity

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function draw() {
     greenValue = random(255);
     blueValue = random(255);
 
-    stroke(redValue, greenValue, blueValue, 50);
+    stroke(redValue, greenValue, blueValue, 1);
 
     if (mouseIsPressed == false) {
         line(random(mouseX), random(mouseY), height / 2, width / 2);
